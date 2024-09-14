131f319a36c9765b8b26f4758504da22b278c284
hakim github <83832346+hakim733@users.noreply.github.com>
2024-09-15 01:09:11 +0200

    changing to stat

--- lab.py
+++ lab.py
@@ -1,20 +1,8 @@
 import numpy as np
 import pandas as pd
-# Create a Pandas DataFrame from a NumPy array
-data = np.array([[0.5, 2.0, 3.0], [4.0, 5.0, np.nan], [7.0, 8.0, 9.0]])
-df = pd.DataFrame(data, columns=['A', 'B', 'C'])
-# Impute missing values using NumPy
-df.fillna(df.mean(), inplace=True)
-# Scale the data using NumPy
-df_scaled = (df - df.mean()) / df.std()
+import statistics
+# Calculate average values
+print(statistics.mean([1, 3, 5, 7, 9, 11, 13]))
+print(statistics.mean([1, 3, 5, 7, 9, 11]))
+print(statistics.mean([-11, 5.5, -3.4, 7.1, -9, 22]))
 
-# Load data and show the top 5 rows
-df = pd.read_csv('house_prices.csv')
-print(df.head())
-# Create X and y arrays
-X = df['sqft'].values.reshape(-1, 1)
-y = df['price'].values.reshape(-1, 1)
-# Compute regression coefficients
-beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-# Print coefficients
-print('Coefficients: ', beta)
